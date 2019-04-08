ce923bd4d5328b6c88fc8552f03783d8903c790e
Rocky Warren <1085683+therockstorm@users.noreply.github.com>
2019-04-08 10:41:27 -0500

    use export statement

--- src/index.ts
+++ src/index.ts
@@ -4,29 +4,21 @@ const l = (fn: any, first: any, ...rest: any[]) => {
   TEST_ENV ? null : first ? fn(first, ...rest) : fn(...rest)
 }
 
-const log = (...args: any[]): void => l(console.log, null, ...args)
-const debug = (...args: any[]): void => l(console.debug, '[debug]', ...args)
-const info = (...args: any[]): void => l(console.info, '[info]', ...args)
-const warn = (...args: any[]): void => l(console.warn, '[warn]', ...args)
-const error = (...args: any[]): void => l(console.error, '[error]', ...args)
+export const log = (...args: any[]): void => l(console.log, null, ...args)
+export const debug = (...args: any[]): void =>
+  l(console.debug, '[debug]', ...args)
+export const info = (...args: any[]): void => l(console.info, '[info]', ...args)
+export const warn = (...args: any[]): void => l(console.warn, '[warn]', ...args)
+export const error = (...args: any[]): void =>
+  l(console.error, '[error]', ...args)
 
-const envVar = (name: string): string => required(process.env[name], name)
+export const envVar = (name: string): string =>
+  required(process.env[name], name)
 
-const required = (val: any, name: string): any =>
+export const required = (val: any, name: string): any =>
   val || thrw(`${name} required`)
 
-const thrw = (err: string | Error): never => {
+export const thrw = (err: string | Error): never => {
   if (err instanceof Error) throw err
   throw new Error(err)
-}
-
-module.exports = {
-  log,
-  debug,
-  info,
-  warn,
-  error,
-  envVar,
-  required,
-  thrw
 }
